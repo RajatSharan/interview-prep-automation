38d9ffbe31d8772c70cafd45647011ae1a973cc5
Rajat Sharan <errajatsharan@gmail.com>
2025-09-12 10:56:54 +0530

    Day6 : Objects

---
+++ src/Day6-Objects/Exercise6_1.ts
@@ -0,0 +1,10 @@
+//Exercise 6.1: Create an object car with properties make, model, and year. Log the model property to the console.
+
+const car:any={
+     make : "AUDI",
+     model: "A1",
+     year: 2022
+
+}
+
+console.log(car.model)
---
+++ src/Day6-Objects/Exercise6_2.ts
@@ -0,0 +1,13 @@
+//Exercise 6.2: Add a new property color to the car object and log the entire object.
+
+const carAdd:any={
+     make : "AUDI",
+     model: "A1",
+     year: 2022,
+     
+
+}
+
+carAdd["color"]="Black"
+
+console.log(carAdd)
